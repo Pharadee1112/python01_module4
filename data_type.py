48b3b25d722a41d983e103e829dca120b0214cbe
Pharadee1112 <Pharadee1112>
2025-10-21 14:42:45 +0700

    add lecture

---
+++ data_type.py
@@ -0,0 +1,29 @@
+'''
+data type 
+- the number (integer)
+3,5,-4 
+- the float (decimal number)
+3.5, -4.6, 0.01, 5.00
+'''
+
+"""
+text type
+- the string (str)
+"hello", 'hi', "1234", "3.5"
+"""
+print(type("0.05")) #type เพื่อที่จะบอกว่าเป็น data type อะไร 
+
+"""
+Ture or False
+- the boolean (bool) (ture value or false value)
+"""
+
+
+"""
+none type
+- the none type (none) there is ni data  the lack of data
+"""
+
+print(type(56)) 
+print(type(4.5))
+print(type("hello"))    
